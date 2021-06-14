29b887cfbae3e326d638eb2ff9d25b0d76714f2a
jojohim <johanna.himstedt.de@gmail.com>
2021-06-14 11:56:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import {useState, useRef} from "react";
 import './App.css';
+import {headers} from "./settings.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Message to Staff</h1>
+    <MessageForm />
     </div>
+
   );
+}
+
+function MessageForm(props){
+
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const [message, setMessage] = useState("");
+  const form = useRef(null);
+
+  function onSubmit(e){
+    console.log(e);
+    e.preventDefault();
+    post({
+      text: message,
+      name: "Zuz",
+      date: `${new Date().getDate()} ${months[new Date().getMonth()]}`,
+    });
+  }
+  return (
+    <section className="messagForm">
+    <form onSubmit={onSubmit} ref={form}>
+    <label for="message">Message</label>
+  <textarea rows="6" placeholder="Your Message" id="message" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
+  <input type="submit" value="send"></input>
+    </form>
+    </section>
+  );
+}
+
+function post(data){
+  console.log(data);
+    const postData = JSON.stringify(data);
+    fetch("https://kea2021-6773.restdb.io/rest/foobar-notes", {
+      method: "post",
+      headers: headers,
+      body: postData,
+    })
+      .then((res) => res.json())
 }
 
 export default App;
---
+++ src/settings.js
@@ -0,0 +1,7 @@
+const apiKey = "60c48116e2c96c46a2463480";
+
+export const headers = {
+    "Content-Type": "application/json; charset=utf-8",
+    "x-apikey": apiKey,
+    "cache-control": "no-cache"
+};
